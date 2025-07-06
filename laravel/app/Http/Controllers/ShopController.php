49b753b984d61aff7d4cddc221faebc6a4d7c056
takaoka sayuko <sp7y3dx9snn@gmail.com>
2025-07-06 11:28:40 +0900

    shopコントローラー作成

---
+++ laravel/app/Http/Controllers/ShopController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Shop;
+use App\Models\Review;
+use Inertia\Inertia;
+
+class ShopController extends Controller
+{
+    public function index()
+    {
+        $shops = Shop::with('reviews')->get();
+        dd($shops);
+        $newReviews = Review::with('shop', 'user')
+            ->orderBy('created_at', 'desc')
+            ->take(5)
+            ->get();
+
+        return Inertia::render('Home', [
+            'shops' => $shops,
+            'newReviews'=>$newReviews,
+        ]);
+    }
+}
